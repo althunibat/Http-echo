f787cbc42a6f065b411ad8a14b0fc610752d3224
Hamza Althunibat <Althunibat@outlook.com>
2019-03-29 15:55:52 +0400

    adjustments

--- HttpEcho.Web/load.sh
+++ HttpEcho.Web/load.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-while : ;do export GREP_COLOR='1;33';curl -s  http://10.196.1.10/ \
- |  grep --color=always "v1" ; export GREP_COLOR='1;36';\
- curl -s  http://10.196.1.10/ \
- | grep --color=always "v2" ; sleep 1; done
+while : 
+do
+    curl -s  http://10.196.1.11/ | GREP_COLOR='01;36' egrep --color=always 'v1' | GREP_COLOR='01;31' egrep -i --color=always 'v2' 
+    sleep 1 
+done
+
